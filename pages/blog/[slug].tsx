e9c9e16ad679361bece1cf3c7bccabc57124b1b0
Fadhli Mulyana <fadhlimulyana@pop-os.localdomain>
2021-08-24 21:49:10 +0700

    Menambahkan title dan open graph pada detail blog

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,12 +1,14 @@
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import Head from 'next/head'
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/dist/client/router";
 import Image from "next/image";
 import MainLayout from "../../components/templates/Main";
 import { blogAPIKey, blogUrl } from "../../utils/api";
 import { DateIndo } from "../../utils/date";
+import OpenGraph from '../../components/atoms/OpenGraph'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     let data = null
@@ -33,8 +35,21 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 export default function BlogPost({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    const router = useRouter()
+    const { slug } = router.query
+
     return (
         <MainLayout>
+            <Head>
+                <title>{data.posts[0].title}</title>
+                <OpenGraph
+                    title={data.posts[0].title}
+                    description={data.posts[0].custom_excerpt || data.posts[0].excerpt}
+                    image={data.posts[0].feature_image}
+                    siteName="Zyklus Indonesia"
+                    url={`https://www.zyklus.id/blog/${slug}`}
+                />
+            </Head>
             <div className="pt-28 lg:px-20 md:px-5 px-2 mb-10">
                 {data ? (
 
@@ -51,7 +66,7 @@ export default function BlogPost({ data }: InferGetServerSidePropsType<typeof ge
                                         ))}
                                     </div>
                                 </div>
-                                <h1 className="md:text-4xl text-2xl font-bold">Sebuah artikel yang bisa dibaca</h1>
+                                <h1 className="md:text-4xl text-2xl font-bold mb-4">{ data.posts[0].title }</h1>
                                 <div className="flex gap-4 items-center mb-8">
                                     {data.posts[0].authors[0].profile_image ? (
                                         <div className="rounded-full h-8 w-8">
--- utils/api.ts
+++ utils/api.ts
@@ -1,2 +1,3 @@
-export const blogUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/ghost/api/v4/' : 'https://ziblog.zyklus.id/ghost/api/v4/'
+// export const blogUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/ghost/api/v4/' : 'https://ziblog.zyklus.id/ghost/api/v4/'
+export const blogUrl = process.env.NODE_ENV === 'development' ? 'https://ziblog.zyklus.id/ghost/api/v4/' : 'https://ziblog.zyklus.id/ghost/api/v4/'
 export const blogAPIKey = process.env.BLOG_API_KEY
